a0eb3a5419cbc403c781df96a805c2bd9b840a9c
Abhinav Shrimali <abhinav@Abhinavs-MacBook-Air.local>
2026-03-01 23:04:26 +0530

    feat: configure frontend api to hit production render backend endpoint

--- sitepulse-frontend/src/api/api.js
+++ sitepulse-frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    // If running logically on localhost, use local. Otherwise, use Render Production URL
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://sitepulse-vdqe.onrender.com",
 });
 
 export const analyzeWebsite = async (data) => {
